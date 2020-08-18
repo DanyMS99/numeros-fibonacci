cc21bd9e623e6ca476cfdb22bd32cda2d4a43940
DanyMS99 <54692475+DanyMS99@users.noreply.github.com>
2020-08-18 16:46:32 -0600

    Fibonacci recursiva

--- Numeros Fibonacci/src/numeros/fibonacci/NumerosFibonacci.java
+++ Numeros Fibonacci/src/numeros/fibonacci/NumerosFibonacci.java
@@ -15,32 +15,29 @@ import java.util.Scanner;
  */
 public class NumerosFibonacci {
     
+    
+  static int Fibonacci(int n){
+      if (n==1){
+          return 0;
+      }
+      if(n==2){
+          return 1;
+      }
+      return Fibonacci(n-1)+Fibonacci(n-2);
+ 
+}
+  
+  
     public static void main(String[] args) {
         
     Scanner sc = new Scanner(System.in);
-    int numero,n,n3;
-    int n1=1;
-    int n2=1;
-    ArrayList NumerosFibonacci = new ArrayList();
-    
-       Scanner leer= new Scanner(System.in);
+    int n;
+    Scanner leer= new Scanner(System.in);
          
         System.out.print("Favor de ingresar un numero: ");
-        numero = leer.nextInt();
+        n = leer.nextInt();
         
-        NumerosFibonacci.add(0);
-        NumerosFibonacci.add(1);
-        NumerosFibonacci.add(2);
-        
-         for(int i =3;i<=numero-1;i++){
-            n2= n1 + n2;
-            n1= n2 - n1;
-            n3=n1 + n2;
-            
-            NumerosFibonacci.add(n3);
-        }
-          System.out.println(NumerosFibonacci);
-         System.out.println("El numero que corresponde a la posición "+ numero+" en la sucesión de Fibonacci es: "+NumerosFibonacci.get((numero-1)));
+         System.out.println("El numero que corresponde a la posición "+ n+" en la sucesión de Fibonacci es: "+ Fibonacci(n));
     }
     
 }
